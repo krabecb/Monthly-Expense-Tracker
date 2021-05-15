5efaa8538b8143a672f6f014a9c01774fb2b3fd4
krabecb <60526718+krabecb@users.noreply.github.com>
2021-05-15 00:02:58 -0500

    Expenses component works

--- src/App.js
+++ src/App.js
@@ -1,9 +1,40 @@
+import React, {useState} from 'react'
 import './App.css';
+import Expenses from './Expenses'
 
-function App() {
+const App = () => {
+  const [name, setName] = useState([])
+  const [appAmount, setAppAmount] = useState([])
+
+  const updateExpenseName = (e) => {
+  	console.log(e.target.value)
+  	setName(e.target.value)
+  }
+
+  const updateAmount = (e) => {
+  	console.log(e.target.value)
+  	setAppAmount(parseInt(e.target.value))
+  }
+
+  const handleSubmit = (e) => {
+  	e.preventDefault()
+  	// setOpen(false)
+  	setName('')
+  	setAppAmount('')
+  	console.log(name)
+  	console.log(typeof(name))
+  	console.log(appAmount)
+  	console.log(typeof(appAmount))
+  }
+
   return (
     <div className="App">
-      <p>Expense Tracker</p>
+      <h1>Expense Tracker</h1>
+      <Expenses 
+      	updateExpenseName={updateExpenseName} 
+      	updateAmount={updateAmount} 
+      	handleSubmit={handleSubmit}
+      />
     </div>
   );
 }
---
+++ src/Expenses/index.js
@@ -0,0 +1,44 @@
+import React, {useState} from 'react'
+import 'semantic-ui-css/semantic.min.css'
+import { Button, Modal, Form, Label } from 'semantic-ui-react'
+
+function Expenses({ handleSubmit, updateExpenseName, updateAmount }) {
+  const [open, setOpen] = useState(false)
+
+  return (
+    <Modal
+      onClose={() => setOpen(false)}
+      onOpen={() => setOpen(true)}
+      open={open}
+      trigger={<Button>Track An Expense</Button>}
+    >
+      <Modal.Header>Track An Expense</Modal.Header>
+      <Modal.Content id="modal-content-expense">
+        <Form onSubmit={(e) => { handleSubmit(e); setOpen(false); }}>
+        	<Label id="expense-name">Expense Name:</Label>
+			<Form.Input
+				type="text"
+				name="expense_name"
+				placeholder="Enter name of expense"
+				onChange={(e) => updateExpenseName(e)}
+			/>
+			<Label id="expense-amount">Amount:</Label>
+			<Form.Input
+				type="number"
+				name="expense-amount"
+				placeholder="Enter amount"
+				onChange={(e) => updateAmount(e)}
+			/>
+
+        	<Modal.Actions>
+          	<Button color='green' id="create-expense" type="submit">
+            	Add Expense
+          	</Button>
+        	</Modal.Actions>
+        </Form>
+      </Modal.Content>
+    </Modal>
+  )
+}
+
+export default Expenses
